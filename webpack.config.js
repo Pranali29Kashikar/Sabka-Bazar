13258db01c75afbb19cb3207fbf80fd900e1563e
Pranali Pradip Kashikar <prakashi@publicisgroupe.net>
2019-03-29 16:02:52 +0530

    webpack set-up

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,32 @@
 const path = require('path');
+const HtmlwebpackPlugin =require('html-webpack-plugin');
 
 module.exports = {
-    entry: './src/js/index.js',
+    entry: ['babel-polyfill','./src/js/index.js'],
     output: {
-        path: path.resolve(__dirname,'resources/js'),
-        filename: 'bundle.js'
+        path: path.resolve(__dirname,'resources'),
+        filename: 'js/bundle.js'
     },
-    mode: 'development' 
+    devServer: {
+        contentBase: './resources'
+    },
+    plugins: [
+        new HtmlwebpackPlugin({
+            filename: 'index.html',
+            template: './src/home.html'
+        })
+    ],
+    
+
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            }
+        ]
+    }
 };
